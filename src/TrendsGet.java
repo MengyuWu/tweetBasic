67a2161c5b1609b13147e6fa81c9935e97f38b32
nb2406@columbia.edu <nb2406@columbia.edu>
2015-11-25 01:19:50 -0500

    Get top 10 location trends for specific place.

---
+++ src/TrendsGet.java
@@ -0,0 +1,93 @@
+import twitter4j.Location;
+import twitter4j.ResponseList;
+import twitter4j.Trends;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public final class TrendsGet {
+
+    public static void main(String[] args) {
+
+	    try {
+	
+			ConfigurationBuilder cb = new ConfigurationBuilder();
+			cb.setDebugEnabled(true)
+			   .setOAuthConsumerKey("xB127kA8Wn91LeZLJNHn7DQLD")
+			   .setOAuthConsumerSecret("XEAEaQBWlDirhoT8noQmbbwDhbBjvjzQcEJIO80eZXIWj6nTKn")
+			   .setOAuthAccessToken("605341080-dSZi4aYnavhiewli3oxRow2aLMpUdv58cqiM5Wmh")
+			   .setOAuthAccessTokenSecret("OIDy5WNOgzcvl1NnbJIneZWTSos3v9CfOeR0NMok8eLCt");
+	
+			TwitterFactory tf = new TwitterFactory(cb.build());
+	        Twitter twitter = tf.getInstance();
+	
+	        ResponseList<Location> locations;
+	        locations = twitter.getAvailableTrends();
+	
+	        Integer idTrendLocation = getTrendLocationId("New York");
+	
+	        if (idTrendLocation == null) {
+	        	System.out.println("Trend Location Not Found");
+	        	System.exit(0);
+	        }
+	
+	        Trends trends = twitter.getPlaceTrends(idTrendLocation);
+	        for (int i = 0; i < trends.getTrends().length; i++) {
+	        	String trend = trends.getTrends()[i].getName();
+	        	System.out.println(trend);
+	        }
+	        
+
+	
+	        System.exit(0);
+	
+	    } catch (TwitterException te) {
+	        te.printStackTrace();
+	        System.out.println("Failed to get trends: " + te.getMessage());
+	        System.exit(-1);
+	    }
+    }
+
+    // Influenced by http://stackoverflow.com/questions/10431321/using-twitter4j-daily-trends
+
+    private static Integer getTrendLocationId(String locationName) {
+
+    int idTrendLocation = 0;
+
+    try {
+
+    	ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+           .setOAuthConsumerKey("xB127kA8Wn91LeZLJNHn7DQLD")
+           .setOAuthConsumerSecret("XEAEaQBWlDirhoT8noQmbbwDhbBjvjzQcEJIO80eZXIWj6nTKn")
+           .setOAuthAccessToken("605341080-dSZi4aYnavhiewli3oxRow2aLMpUdv58cqiM5Wmh")
+           .setOAuthAccessTokenSecret("OIDy5WNOgzcvl1NnbJIneZWTSos3v9CfOeR0NMok8eLCt");
+        
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+
+        ResponseList<Location> locations;
+        locations = twitter.getAvailableTrends();
+        
+        for (Location location : locations) {
+	        if (location.getName().toLowerCase().equals(locationName.toLowerCase())) {
+	            idTrendLocation = location.getWoeid();
+	            break;
+	        }
+        }
+
+        if (idTrendLocation > 0) {
+        	return idTrendLocation;
+        }
+
+        return null;
+
+    } catch (TwitterException te) {
+        te.printStackTrace();
+        System.out.println("Failed to get trends: " + te.getMessage());
+        return null;
+    }
+
+    }
+}
