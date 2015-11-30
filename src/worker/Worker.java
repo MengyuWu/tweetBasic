56015d86ee18f6b17feeede5c722c43aad109f5e
nb2406@columbia.edu <nb2406@columbia.edu>
2015-11-30 13:38:42 -0500

    Worker either deletes or marks unsentimental tweets.

--- src/worker/Worker.java
+++ src/worker/Worker.java
@@ -6,12 +6,8 @@ import static tweetBasic.AWSResourceSetup.DYNAMODB_MAPPER;
 import static tweetBasic.AWSResourceSetup.SNS_TOPIC_ARN;
 import static tweetBasic.AWSResourceSetup.SNS;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Date;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
@@ -59,16 +55,14 @@ public class Worker extends Thread {
     
     @Override
     public void run() {
-        System.out.println("Worker listening for work");
+        System.out.println("[Worker] Worker listening for work");
         String queueUrl = SQS.getQueueUrl(new GetQueueUrlRequest(SQS_QUEUE_NAME)).getQueueUrl();
 
         while (true) {
             try {
-                ReceiveMessageResult result = SQS.receiveMessage(
-                        new ReceiveMessageRequest(queueUrl).withMaxNumberOfMessages(1));
+                ReceiveMessageResult result = SQS.receiveMessage(new ReceiveMessageRequest(queueUrl).withMaxNumberOfMessages(1));
                 for (Message msg : result.getMessages()) {
-                	
-                	//put into the working thread pool
+                	// Put into the working thread pool
                     executorService.submit(new MessageProcessor(queueUrl, msg));
                 }
                 sleep(1000);
@@ -106,26 +100,22 @@ public class Worker extends Thread {
         @Override
         public void run() {
             String id = msg.getBody();
-            System.out.println("MSG: "+msg.getBody()+" tID"+Thread.currentThread().getId());
+            System.out.println("[Worker] MSG: "+msg.getBody()+" thread ID: "+Thread.currentThread().getId());
             Tweet tweet=null;
 			try {
 				tweet = DYNAMODB_MAPPER.load(Tweet.class, id);
 			} catch (Exception e1) {
-				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 
-            System.out.println("test");
             try {
             	
-            	if(tweet==null){
-            		System.out.println("tweet is null");
-            	}
-            	
-                if (tweet != null) {
-                	System.out.println("tweet not null");
-                	//Process Content, save back to db
-                	 // Extract sentiment for a text string.
+            	if (tweet == null) {
+            		System.out.println("[Worker] tweet is null");
+            	} else {
+                	System.out.println("[Worker] tweet not null");
+                	// Process Content, save back to db
+                	// Extract sentiment for a text string.
                     Document doc;
                     String category = "";
                     String sentiment = "";
@@ -135,48 +125,48 @@ public class Worker extends Thread {
     					sentiment = getSentiment(doc);
     					doc = alchemyObj.TextGetCategory(text);
     					category = getCategory(doc);
-    					
-    					System.out.println("text"+text+" category "+category+" sentiment "+sentiment);
-    					
     				} catch (XPathExpressionException e) {
     				} catch (IOException e) {
     				} catch (SAXException e) {
     				} catch (ParserConfigurationException e) {
     				}
 
-
 					// Log tweet details. User - Date - (Lat, Long) - Content - Sentiment - Category.
                     System.out.println(tweet.getUsername()
                     		+ " on " + tweet.getCreatedDate()
                     		+ " at (" + tweet.getGeoLat() + ", " + tweet.getGeoLng() + ")\n"
                     		+ tweet.getContent());
+                    
                     if (sentiment.length() != 0 && category.length() != 0) {
                     	tweet.setCategory(category);
                     	tweet.setSentiment(sentiment);
-                    	System.out.println("sentiment: " + sentiment + "; " + "category: " + category);
+                    	System.out.println("[Worker] sentiment: " + sentiment + "; " + "category: " + category);
+                        System.out.println("[Worker] save tweet");
+                        DYNAMODB_MAPPER.save(tweet);
+                        
+                        // Publish a notification to indicate a new tweet is processed
+                        PublishRequest publishReq = new PublishRequest()
+                        .withTopicArn(SNS_TOPIC_ARN)
+                        .withMessage(tweet.getId());
+                        SNS.publish(publishReq);
+                        System.out.println("[Worker] publish notification");
+                        
                     } else {
-                    	System.out.println("no sentiment; no category");
+                    	tweet.setCategory("uncategorizable");
+                    	tweet.setSentiment("unsentimental");
+                    	System.out.println("[Worker] no sentiment; no category");
+                    	// System.out.println("[Worker] delete tweet " + id);
+                        // DYNAMODB_MAPPER.delete(tweet);
                     }
                     
-                    System.out.println("save tweet");
-                    DYNAMODB_MAPPER.save(tweet);
-                    
-                    //publish a notification to indicate a new tweet is processed
-                    PublishRequest publishReq = new PublishRequest()
-                    .withTopicArn(SNS_TOPIC_ARN)
-                    .withMessage(tweet.getId());
-                    SNS.publish(publishReq);
-                    System.out.println("publish notification");
-                Thread.sleep(1000);
-                    
-                   
+                    Thread.sleep(1000);                  
                 }
             } catch (Exception e) {
                 e.printStackTrace();
                 
             }
             
-            System.out.println("delete queueurl");
+            System.out.println("[Worker] delete queue url");
             SQS.deleteMessage(new DeleteMessageRequest(queueUrl, msg.getReceiptHandle()));
         }
         
